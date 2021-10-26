67ccef061b749bd0e0217a28ede33d5adf2351d8
dnussipa <daniyal_nussipakynov@sfu.ca>
2021-10-26 01:03:40 -0700

    Add to ChildrenManager
    Not sure, if this is 100% correct, just sort of an idea of how to do this. We should discuss in meeting to decide on the full functionality of this class.

--- app/src/main/java/com/example/practicalparentapp/Model/ChildrenManager.java
+++ app/src/main/java/com/example/practicalparentapp/Model/ChildrenManager.java
@@ -1,4 +1,7 @@
 package com.example.practicalparentapp.Model;
+import java.util.List;
+import java.util.ArrayList;
+
 
 /**
  * This class is responsible for allowing the parent
@@ -6,4 +9,32 @@ package com.example.practicalparentapp.Model;
  * Includes adding, removing, and editing children.
  */
 public class ChildrenManager {
+
+    private List<String> children = new ArrayList<>();
+
+    private static ChildrenManager instance;
+
+    public static ChildrenManager getInstance() {
+        if (instance == null) {
+            instance = new ChildrenManager();
+        }
+        return instance;
+    }
+
+    private ChildrenManager() {
+        // nothing to ensure this is a singleton
+    }
+
+    public void addChild (String name) {
+        String childName = name;
+        children.add(childName);
+    }
+
+    public void removeChild (String name) {
+        String toBeRemoved=name;
+        children.remove(toBeRemoved);
+    }
+
+
+
 }
