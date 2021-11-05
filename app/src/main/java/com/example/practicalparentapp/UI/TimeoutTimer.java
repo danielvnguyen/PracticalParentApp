6b7ada904c10d8153690ec5482025921087eaa7d
emmanuel_okonkwo <chukwujekwuekene2017@gmail.com>
2021-11-05 11:40:15 -0700

    Finished with the duration set-up for other timeout time intervals

--- app/src/main/java/com/example/practicalparentapp/UI/TimeoutTimer.java
+++ app/src/main/java/com/example/practicalparentapp/UI/TimeoutTimer.java
@@ -6,9 +6,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,15 +22,15 @@ import java.util.Locale;
 public class TimeoutTimer extends AppCompatActivity {
 
     private long START_TIME_IN_MILLIS;
-    private TextView mTextViewCountDown;
-    private Button mButtonStartPause;
-    private Button mButtonReset;
+    private TextView mTextViewCountDown, timeText;
+    private Button mButtonStartPause, mButtonReset, mButtonSave;
+    private EditText inputTime;
 
     private CountDownTimer mCountDownTimer;
 
     private boolean mTimerRunning;
 
-    private long mTimeLeftInMillis= START_TIME_IN_MILLIS;
+    private long mTimeLeftInMillis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,28 +52,60 @@ public class TimeoutTimer extends AppCompatActivity {
                 getResources().getStringArray(R.array.timeout_minutes)));
 
         mySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                        @Override
-                        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                            switch (i) {
-                                case 0:
-                                    Toast.makeText(TimeoutTimer.this, "Message?", Toast.LENGTH_SHORT).show();
-                                    break;
-                                case 1:
-                                    Toast.makeText(TimeoutTimer.this, "Message?", Toast.LENGTH_SHORT).show();
-                                    break;
-                                case 2:
-                                    Toast.makeText(TimeoutTimer.this, "Message?", Toast.LENGTH_SHORT).show();
-                                    break;
-                                case 3:
-                                    Toast.makeText(TimeoutTimer.this, "Message?", Toast.LENGTH_SHORT).show();
-                                    break;
-                                case 4:
-                                    Toast.makeText(TimeoutTimer.this, "Message?", Toast.LENGTH_SHORT).show();
-                                    break;
-                                case 5:
-                                    Toast.makeText(TimeoutTimer.this, "Message?", Toast.LENGTH_SHORT).show();
-                                    break;
-                                default:
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i) {
+                    case 0:
+                        setInputVisiblityToTrue();
+                        START_TIME_IN_MILLIS = 60000;
+                        mTimeLeftInMillis= START_TIME_IN_MILLIS;
+                        updateCountDownText();
+                        break;
+                    case 1:
+                        setInputVisiblityToTrue();
+                        START_TIME_IN_MILLIS = 120000;
+                        mTimeLeftInMillis= START_TIME_IN_MILLIS;
+                        updateCountDownText();
+                        break;
+                    case 2:
+                        setInputVisiblityToTrue();
+                        START_TIME_IN_MILLIS = 180000;
+                        mTimeLeftInMillis= START_TIME_IN_MILLIS;
+                        updateCountDownText();
+                        break;
+                    case 3:
+                        setInputVisiblityToTrue();
+                        START_TIME_IN_MILLIS = 300000;
+                        mTimeLeftInMillis= START_TIME_IN_MILLIS;
+                        updateCountDownText();
+                        break;
+                    case 4:
+                        setInputVisiblityToTrue();
+                        START_TIME_IN_MILLIS = 600000;
+                        mTimeLeftInMillis= START_TIME_IN_MILLIS;
+                        updateCountDownText();
+                        break;
+                    case 5:
+                        mTextViewCountDown.setVisibility(View.INVISIBLE);
+                        mButtonStartPause.setVisibility(View.INVISIBLE);
+                        timeText = findViewById(R.id.textTime);
+                        timeText.setVisibility(View.VISIBLE);
+                        inputTime = findViewById(R.id.editTextNumber);
+                        inputTime.setVisibility(View.VISIBLE);
+                        mButtonSave = findViewById(R.id.button_save);
+                        mButtonSave.setVisibility(View.VISIBLE);
+                        mButtonSave.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                int minutes = Integer.parseInt(inputTime.getText().toString());
+                                setInputVisiblityToTrue();
+                                START_TIME_IN_MILLIS = minutes*60*1000;
+                                mTimeLeftInMillis= START_TIME_IN_MILLIS;
+                                updateCountDownText();
+                               }
+                        });
+                        break;
+                    default:
                         break;
                 }
             }
@@ -81,6 +115,17 @@ public class TimeoutTimer extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void setInputVisiblityToTrue() {
+        mTextViewCountDown.setVisibility(View.VISIBLE);
+        mButtonStartPause.setVisibility(View.VISIBLE);
+        timeText = findViewById(R.id.textTime);
+        timeText.setVisibility(View.INVISIBLE);
+        inputTime = findViewById(R.id.editTextNumber);
+        inputTime.setVisibility(View.GONE);
+        mButtonSave = findViewById(R.id.button_save);
+        mButtonSave.setVisibility(View.INVISIBLE);
     }
 
     private void timeoutDropdown() {
@@ -158,7 +203,4 @@ public class TimeoutTimer extends AppCompatActivity {
 
         mTextViewCountDown.setText(timeLeftFormatted);
     }
-
-
-
 }
